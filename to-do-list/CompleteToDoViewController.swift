a0d5bcf18a40e78efd69423ffb6ada222b792202
Erika <erikaec2100@gmail.com>
2022-07-19 11:48:11 -0700

    Initial Commit

---
+++ to-do-list/CompleteToDoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CompleteToDoViewController.swift
+//  to-do-list
+//
+//  Created by erika on 7/18/22.
+//
+
+import UIKit
+
+class CompleteToDoViewController: UIViewController {
+    
+    var previousVC = ToDoTableViewController()
+    var selectedToDo : ToDoCD?
+
+  @IBOutlet weak var titleLabel: UILabel!
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+
+        titleLabel.text = selectedToDo?.name
+    }
+
+    @IBAction func completeTapped(_ sender: Any) {
+      if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+        if let theToDo = selectedToDo {
+          context.delete(theToDo)
+          navigationController?.popViewController(animated: true)
+        }
+      }
+    }
+}
---
+++ to-do-list/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  to-do-list
+//
+//  Created by erika on 7/18/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
